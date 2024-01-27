fix model output precision

mlora/model.py
@@ -1,7 +1,6 @@
 from mlora.modelargs import KVCache, LoraConfig, MultiLoraBatchData, LLMModelOutput
 
 import torch
-import einops
 
 from abc import ABCMeta, abstractclassmethod
 from typing import Tuple, Dict, List, Optional
@@ -32,22 +31,20 @@ def precompute_mask(tokens: torch.Tensor,
 
 
 def precompute_rope_angle(dim: int, seq_len: int, device: str, theta: float = 10000.0) -> Tuple[torch.Tensor, torch.Tensor]:
-    angles = 1.0 / (theta ** (torch.arange(0, dim, 2).to(device)
-                              [: (dim // 2)].to(torch.float) / dim))
-    seq = torch.arange(seq_len, device=angles.device)
-    emb = torch.outer(seq, angles).float()
-    emb = einops.repeat(emb, "... n -> ... (n r)", r=2)
+    inv_freq = 1.0 / \
+        (theta ** (torch.arange(0, dim, 2).float().to(device) / dim))
+    t = torch.arange(seq_len, device=device, dtype=inv_freq.dtype)
+    freqs = torch.outer(t, inv_freq)
+    emb = torch.cat((freqs, freqs), dim=-1)
 
-    emb.requires_grad_(False)
     # cos(angle), sin(angle)
-    return (emb.cos().to(torch.float32), emb.sin().to(torch.float32))
+    return (emb.cos().float(), emb.sin().float())
 
 
-def rotate_half(x: torch.Tensor) -> torch.Tensor:
-    x = einops.rearrange(x, "... (d r) -> ... d r", r=2)
-    x1, x2 = x.unbind(dim=-1)
-    x = torch.stack((-x2, x1), dim=-1)
-    return einops.rearrange(x, "... d r -> ... (d r)")
+def rotate_half(x):
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2:]
+    return torch.cat((-x2, x1), dim=-1)
 
 
 def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
@@ -65,12 +62,12 @@ def apply_rotary_emb(xq: torch.Tensor, xk: torch.Tensor,
     # data shape is: batch_size * max_seq_len * n_head * n_dim
     _, max_seq_len, _, dim_head = xq.shape
 
-    cos = angle[0][:max_seq_len].view(max_seq_len, 1, dim_head)
-    sin = angle[1][:max_seq_len].view(max_seq_len, 1, dim_head)
+    cos = angle[0][:max_seq_len].view(max_seq_len, 1, dim_head).to(xq.dtype)
+    sin = angle[1][:max_seq_len].view(max_seq_len, 1, dim_head).to(xq.dtype)
 
-    xq = (xq * cos) + (rotate_half(xq) * sin)
-    xk = (xk * cos) + (rotate_half(xk) * sin)
-    return (xq.to(dtype), xk.to(dtype))
+    q_embed = (xq * cos) + (rotate_half(xq) * sin)
+    k_embed = (xk * cos) + (rotate_half(xk) * sin)
+    return q_embed, k_embed
 
 
 def apply_rotary_emb_to_one(x: torch.Tensor, angle: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:

mlora/model_llama.py
@@ -44,15 +44,12 @@ class RMSNormLayer(torch.nn.Module):
         self.norm_eps_ = eps
         self.weight_ = weight
 
-    def _norm(self, data: torch.Tensor) -> torch.Tensor:
-        return data * torch.rsqrt(+ self.norm_eps_)
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.norm_eps_)
 
-    def forward(self, data: torch.Tensor) -> torch.Tensor:
-        input_dtype = data.dtype
-        v = data.to(torch.float32).pow(2).mean(-1, keepdim=True)
-        data = data * torch.rsqrt(v + self.norm_eps_)
-
-        return (self.weight_ * data).to(input_dtype)
+    def forward(self, x: torch.Tensor):
+        output = self._norm(x.float()).to(x.dtype)
+        return output * self.weight_
 
 
 class CasualOutputLayer(LLMOutput):
@@ -62,7 +59,7 @@ class CasualOutputLayer(LLMOutput):
         self.lm_head_: torch.nn.Linear = weight
 
     def forward(self, data: torch.Tensor) -> torch.Tensor:
-        return self.lm_head_(data.to(torch.float32))
+        return self.lm_head_(data).float()
 
     def loss(self,
              input_ids: torch.Tensor,
@@ -336,7 +333,7 @@ class LlamaModel(LLMModel):
 
         self.norm_: RMSNormLayer = None    # dim
         self.lm_head_ = torch.nn.Linear(
-            args.dim_, args.vocab_size_, bias=False, device=args.device_, dtype=torch.float32)
+            args.dim_, args.vocab_size_, bias=False, device=args.device_, dtype=args.dtype_)
         self.output_: OutputLayer = OutputLayer()
 
         # cos and sin
@@ -424,6 +421,8 @@ class LlamaModel(LLMModel):
                 device=self.device_,
                 weight=weight)
         self.output_.layers_[config.adapter_name_] = output_layer
+        if config.adapter_name_ == "default":
+            return
         # init transformer layers
         for transformer_layer in self.layers_:
             transformer_layer.init_lora_layer_weight(config, weight)
@@ -645,12 +644,16 @@ class LlamaModel(LLMModel):
     def load_adapter_weight(self, path: str, adapter_name: str = None):
         if adapter_name is None:
             adapter_name = path
-        if not os.path.exists(path):
-            path = snapshot_download(repo_id=path, repo_type="model")
-        with open(path + os.sep + "adapter_config.json", 'r', encoding='utf8') as fp:
-            lora_config = lora_config_factory(json.load(fp))
-        lora_config.adapter_name_ = adapter_name
-        lora_weight = torch.load(
-            path + os.sep + "adapter_model.bin", map_location=self.device_)
+        if path != "default":
+            if not os.path.exists(path):
+                path = snapshot_download(repo_id=path, repo_type="model")
+            with open(path + os.sep + "adapter_config.json", 'r', encoding='utf8') as fp:
+                lora_config = lora_config_factory(json.load(fp))
+            lora_config.adapter_name_ = adapter_name
+            lora_weight = torch.load(
+                path + os.sep + "adapter_model.bin", map_location=self.device_)
+        else:
+            lora_config = LoraConfig(adapter_name_=path)
+            lora_weight = None
         self.init_lora_layer_weight(lora_config, lora_weight)
         return adapter_name

mmlu_evaluate.py
@@ -38,7 +38,8 @@ def prepare_data(tokenizer: mlora.Tokenizer,
                  dev_data: datasets.Dataset,
                  test_data: datasets.Dataset,
                  k_shots=5,
-                 max_seq_len=2048):
+                 max_seq_len=2048,
+                 batch_padding=True):
 
     sequence_lengths = []
     batch_tokens = []
@@ -71,15 +72,19 @@ def prepare_data(tokenizer: mlora.Tokenizer,
         batch_tokens.append(tokens)
         batch_labels.append(test_data_point["answer"])
 
-    logging.info(f"Max tokens: {max_tokens_len}/{max_seq_len}")
-    if max_tokens_len < max_seq_len:
-        max_seq_len = math.ceil(max_tokens_len / 8) * 8
-    logging.info(f"Max sequence length: {max_seq_len}")
+    if batch_padding:
+        logging.info(f"Max tokens: {max_tokens_len}/{max_seq_len}")
+        if max_tokens_len < max_seq_len:
+            max_seq_len = math.ceil(max_tokens_len / 8) * 8
+        logging.info(f"Max sequence length: {max_seq_len}")
 
     for tokens in batch_tokens:
-        sequence_lengths.append(len(tokens) - 1)
-        while len(tokens) < max_seq_len:
-            tokens.append(tokenizer.pad_id_)
+        if batch_padding:
+            sequence_lengths.append(len(tokens) - 1)
+            while len(tokens) < max_seq_len:
+                tokens.append(tokenizer.pad_id_)
+        else:
+            sequence_lengths.append(-1)
         atten_masks.append(tokenizer.attention_mask(tokens))
 
     return sequence_lengths, batch_tokens, atten_masks, batch_labels
@@ -97,7 +102,7 @@ def evaluate(subject: str,
     mmlu = datasets.load_dataset("cais/mmlu", subject)
 
     sequence_lengths, batch_tokens, atten_masks, batch_labels = prepare_data(
-        tokenizer, subject, mmlu["dev"], mmlu["test"], 5, max_seq_len)
+        tokenizer, subject, mmlu["dev"], mmlu["test"], 5, max_seq_len, batch_size > 1)
 
     # load adapters
 
@@ -137,6 +142,7 @@ def evaluate(subject: str,
             batch_tokens_=batch_tokens[start_pos:end_pos]*len(adapter_names),
             attention_masks_=atten_masks[start_pos:end_pos]*len(adapter_names),
             gradient_checkpoint_=False,
+            inference_seq_pos_=-1 if batch_size > 1 else 0,
         )
 
         outputs = model.forward(input_args)
@@ -148,7 +154,7 @@ def evaluate(subject: str,
             logits = output.logits
             logits = logits[torch.arange(
                 bsz, device=logits.device), sequence_lengths[start_pos:end_pos]]
-            logits = logits[:, label_indices].float()
+            logits = logits[:, label_indices]
             logits = logits.softmax(-1).argmax(-1)
             result = (logits == labels).int().tolist()
             results[output.adapter_name].extend(result)
@@ -234,7 +240,7 @@ mmlu_categories = {
 model_dtypes = {
     "4bit": {"bits": 4, "load_dtype": torch.float32},
     "8bit": {"bits": 8, "load_dtype": torch.float32},
-    "16bit": {"load_dtype": torch.bfloat16},
+    "16bit": {"load_dtype": torch.float16},
 }
 
 